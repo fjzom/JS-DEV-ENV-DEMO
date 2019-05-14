d07ead6fc28c6ff71c241fbc75ef234590d3f3ca
Oviedo Maltez <f.oviedo.maltez@accenture.com>
2019-05-13 21:59:19 -0500

    Adding cashe busting, hashing, css bundling and loggers

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,6 @@
 import path from 'path';  
-import HtmlWebpackPlugin from 'html-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';  
+import  MiniCssExtractPlugin from 'mini-css-extract-plugin'; 
 export default { 
   devtool: 'source-map', 
   entry: {
@@ -15,9 +16,11 @@ export default {
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/',
-    filename: '[name].js'
+    filename: '[name].[chunkhash].js'
   }, 
     plugins: [
+      // Generate an external css file with a hash in the filename 
+      new MiniCssExtractPlugin({filename: '[name].[chunkhash].css'}),
       // Create HTML file that includes reference to bundled JS.
       new HtmlWebpackPlugin({
         template: './src/index.html' ,
@@ -33,14 +36,16 @@ export default {
           minifyCSS: true,
           minifyURLs: true
         },
-        inject: true 
+        inject: true ,        
+      // Properties you define here are available in index.html
+      // using htmlWebpackPlugin.options.varName
+        tokenJSToken: '69cba16e36d04240a54ba0d664a8c845'
       })
     ],
   module: {
     rules: [
       {  test: /\.jsx$/,  loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.css$/, use: ["style-loader", "css-loader"]}
-    ]
+      { test: /\.css$/, use:[ MiniCssExtractPlugin.loader, 'css-loader'] }    ]
   }
 }
  
